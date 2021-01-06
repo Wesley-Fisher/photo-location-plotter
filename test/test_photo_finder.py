caccc40e6b75a0289f300c71af8ba7bc16c6605d
Wesley Fisher <wesley_b_fisher@hotmail.com>
2021-01-06 17:59:55 -0400

    Add tests for rel and abs directories

--- test/test_photo_finder.py
+++ test/test_photo_finder.py
@@ -13,3 +13,21 @@ class TestPhotoFinder(unittest.TestCase):
                          helper.get_test_FileSystemHelper(),
                          helper.logger())
         self.assertEqual(len(pf.get_all_files()), 21)
+
+    def test_absolute_directory(self):
+        conf = helper.get_test_ConfigSettings()
+        abs_dir = helper.get_app_dir() + "/projects/test_project/photos/subfolder_a"
+        conf.config['photo_directories'] = [abs_dir]
+        pf = PhotoFinder(conf,
+                         helper.get_test_FileSystemHelper(),
+                         helper.logger())
+        self.assertEqual(len(pf.get_all_files()), 8)
+
+    def test_relative_directory(self):
+        conf = helper.get_test_ConfigSettings()
+        rel_dir = '$PROJECT/photos/subfolder_b'
+        conf.config['photo_directories'] = [rel_dir]
+        pf = PhotoFinder(conf,
+                         helper.get_test_FileSystemHelper(),
+                         helper.logger())
+        self.assertEqual(len(pf.get_all_files()), 6)
