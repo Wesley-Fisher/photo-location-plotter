d8dd2f14343d13326cbe6eeb455a197844c41762
Wesley Fisher <wesley_b_fisher@hotmail.com>
2021-01-07 12:11:43 -0400

    Add some config, and clean up image

--- photo_location_plotter/plotter.py
+++ photo_location_plotter/plotter.py
@@ -9,19 +9,29 @@ class Plotter:
 
         background = plt.imread(in_file)
 
-        fig, ax = plt.subplots(figsize=(8,7))
+        fig, ax = plt.subplots()
+        fig.tight_layout(pad=0.0)
 
         longs = [p.lon for p in points]
         lats = [p.lat for p in points]
 
-        ax.scatter(longs, lats, zorder=1, alpha= 0.3, c='b', s=25)
+        styling = config.config.get('styling', {})
+
+        ax.scatter(longs, lats, alpha=styling.get('point_alpha', 0.3), c=styling.get('point_color', 'b'), s=styling.get('point_size', 25))
 
         bounds = config.config['map']['bounds']
         ax.set_xlim(bounds['longitude'][0], bounds['longitude'][1])
         ax.set_ylim(bounds['latitude'][0], bounds['latitude'][1])
-
         BBox = (bounds['longitude'][0], bounds['longitude'][1], bounds['latitude'][0], bounds['latitude'][1])
+
+        ax.set_xticklabels([])
+        ax.set_xticks([])
+        ax.set_yticklabels([])
+        ax.set_yticks([])
+
+        plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0)
 
         ax.imshow(background, zorder=0, extent=BBox, aspect='equal')
         out_file = file_system_helper.get_project_directory() + "/" + config.config['map']['out_file']
-        plt.savefig(out_file, dpi=800)
+
+        plt.savefig(out_file, dpi=800, bbox_inches='tight', pad_inches=0.1)
